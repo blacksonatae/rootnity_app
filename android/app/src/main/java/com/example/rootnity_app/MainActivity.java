d62f31f79cac481f0f20ad29d221174881227493
chizururu <167151222+blacksonatae@users.noreply.github.com>
2025-01-06 07:15:30 +0700

    Initial commit

---
+++ android/app/src/main/java/com/example/rootnity_app/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.rootnity_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/user.dart
@@ -0,0 +1,14 @@
+class User {
+  final String email;
+  final String name;
+  final String password;
+
+  // Constructor
+  User({
+    required this.email,
+    required this.name,
+    required this.password,
+  });
+
+
+}
