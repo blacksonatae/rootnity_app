0e377681e9bde31288b811aa5afa336bf66f3f19
brodytiktok <167151222+blacksonatae@users.noreply.github.com>
2025-01-06 21:08:08 +0700

    Make Login

---
+++ lib/screens/Auth/Register.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Register extends StatelessWidget {
+  const Register ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+
+      ),
+    );
+  }
+}
