6d68ed04496619e47b29186425f363c80fe6da45
chizururu <167151222+blacksonatae@users.noreply.github.com>
2025-01-13 06:39:46 +0700

    Make Authentication UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:rootnity_app/screens/login.dart';
+import 'package:rootnity_app/screens/register.dart';
 
 void main() {
-  FlutterBluePlus.setLogLevel(LogLevel.verbose, color: false);
-
-  FlutterBluePlus.logs.listen((String log) {
-    debugPrint("Bluetooth Log: $log");
-  });
-
   runApp(const MyApp());
 }
 
@@ -17,58 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen(),
-    );
-  }
-}
-
-class MainScreen extends StatefulWidget {
-  const MainScreen({super.key});
-
-  @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  String status = "Unknown"; // Status default
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Mendengarkan status adapter Bluetooth
-    FlutterBluePlus.adapterState.listen((BluetoothAdapterState state) {
-      debugPrint("Bluetooth State: $state");
-
-      // Perbarui status berdasarkan perubahan state Bluetooth
-      setState(() {
-        if (state == BluetoothAdapterState.on) {
-          status = "Bluetooth is connected";
-        } else {
-          status = "Bluetooth is not connected";
-        }
-      });
-    });
-
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Bluetooth Log Example"),
-      ),
-      body: Center(
-        child: Text(
-          status, // Menampilkan status Bluetooth di layar
-          style: const TextStyle(fontSize: 20),
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const SafeArea(child: Register()),
     );
   }
 }
