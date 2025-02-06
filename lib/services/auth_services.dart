ad5dd8852ba89ac8f06a51546b0edfd7eb04b48c
blacksonatae <167151222+blacksonatae@users.noreply.github.com>
2025-02-06 19:29:22 +0700

    Update auth_services.dart

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -2,14 +2,26 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class AuthService {
-  final String baseUrl = "";
+  final String baseUrl = "https://8c53-2407-0-3002-4817-e9aa-f380-912c-97f6.ngrok-free.app/";
 
   /*
   * Register pengguna dengan input nama pengguna, email, password, dan confirmation password
   * */
+  Future<Map<String, dynamic>?> register(String name, String email, String password, String confirmPassword) async {
+    // response
+    final response = await http.post(
+      Uri.parse('$baseUrl/register'),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode(object),
+    );
+    return null;
+  }
 
-
-/*
+  /*
   * Login pengguan dengan input email dan password
   * */
+  Future<Map<String, dynamic>?> login(String email, String password) async {
+
+    return null;
+  }
 }
