c8691637e1fcae1deeb776d64825556465ca99d3
chizururu <167151222+blacksonatae@users.noreply.github.com>
2025-04-08 07:51:39 +0700

    oke

--- lib/core/model/sectors.dart
+++ lib/core/model/sectors.dart
@@ -1,1 +1,23 @@
-class Sectors {}
+class Sectors {
+  final int id;
+  final String name;
+
+  //.. Constuctor pada sectors untuk menyimpan data sektor
+  Sectors({required this.id, required this.name});
+
+  //.. Convert JSON ke Object
+  factory Sectors.fromJson(Map<String, dynamic> json) {
+    return Sectors(
+      id: json['id'] ?? 0,
+      name: json['name_sectors'] ?? '',
+    );
+  }
+
+  //.. Convert Object to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'name_sectors': name,
+    };
+  }
+}
